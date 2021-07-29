28d89f24bc625e3dbcc4a37cc927d4e6c4961465
Crumbleberry <u1038371@utah.edu>
2021-07-28 20:56:49 -0600

    Added the login and Create Account pages

--- index.js
+++ index.js
@@ -24,10 +24,57 @@ app.get('/', (req, res) => {
     res.render('LoginPage');
 })
 
-// POST Routes
-app.post('/login', (req,res) => {
-    res.send('Success');
+app.get('/createaccount', (req, res) => {
+    res.render('createaccount');
 })
 
+app.get('/:id/landing', (req, res) => {
+    res.render('landing');
+})
+
+// POST Routes
+app.post('/login', (req,res) => {
+    var enteredUsername = '';
+
+    User.find({userName: req.params.userName})
+    .then((result) => {
+        if(result) {
+            res.redirect(`/${result.userID}/landing`);
+        } else {
+            res.send('Invalid Username or Password');
+        }
+    })
+})
+
+app.post('/createaccount', async (req, res) => {
+    var maxID = 0;
+
+    await User.find({userName: req.params.userName})
+    .then(async (result) => {
+        if(result) {
+            res.send('This username is already in user');
+        } else {
+            await User.find()
+            .then((laterResult) => {
+                laterResult.foreach((user) => {
+                    if(user.userID > maxID) {
+                        maxID = user.userID;
+                    }
+                })
+            })
+
+            const newUser = new User({
+                userName: req.params.userName,
+                userPassword: req.params.userPassword,
+                userID: maxID + 1
+            })
+
+            User.save()
+            .then((result) => {
+                res.redirect(`/${result.userID}/landing`);
+            })
+        }
+    })
+})
 
 app.listen(3000);
