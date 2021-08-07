6653ca4474d682d61d80e182ac11c2eda8f824ac
Crumbleberry <u1038371@utah.edu>
2021-08-07 11:56:51 -0600

    Connected to AWS DynamoDB

--- index.js
+++ index.js
@@ -3,21 +3,52 @@ const express = require('express');
 const app = express();
 app.set('view engine','ejs');
 
-// Setting up the Database using MongoDB
-const mongoose = require('mongoose');
+//Setting up the body parser
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: true})); 
 
-// Setting up the Database URL
-const dbURL = 'mongodb+srv://user1:UUTo7oqGN58dZqE3@timeclock.oaikt.mongodb.net/TimeClock?retryWrites=true&w=majority';
+// Setting up the Database using DynamoDB
+const AWS = require('aws-sdk');
+
+// Setting up the AWS Config
+const awsConfig = {
+    'region': 'us-east-2',
+    'endpoint': 'http://dynamodb.us-east-2.amazonaws.com',
+    'accessKeyId': 'AKIATAGDZM7M4XE32Q7U', 'secretAccessKey': '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'
+}
+
+AWS.config.update(awsConfig);
+
 
 //Connecting to the DB
-mongoose.connect(dbURL)
-    .then((result) => console.log('Connected to db'))
-    .catch((err) => console.log(err));
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+//testing
+let testingFunction = function() {
+
+    var newItem = {
+        'UserID': 1,
+        'UserName': 'Tyler'
+    };
+
+    var params = {
+        TableName: 'MessagingAppUsers',
+        Item: newItem
+    };
+
+    docClient.put(params, (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Saved!');
+        }
+    })
+
+};
+
+//testingFunction();
 
 // Bringing in User and Message Schema
-const Message = require('./Models/Messages');
-const User = require('./Models/Users');
-
 
 // GET Routes
 app.get('/', (req, res) => {
@@ -36,45 +67,58 @@ app.get('/:id/landing', (req, res) => {
 app.post('/login', (req,res) => {
     var enteredUsername = '';
 
-    User.find({userName: req.params.userName})
-    .then((result) => {
-        if(result) {
-            res.redirect(`/${result.userID}/landing`);
-        } else {
-            res.send('Invalid Username or Password');
-        }
-    })
+
 })
 
 app.post('/createaccount', async (req, res) => {
     var maxID = 0;
+    
+    console.log('Pulling Users for ID');
 
-    await User.find({userName: req.params.userName})
-    .then(async (result) => {
-        if(result) {
-            res.send('This username is already in user');
+    var params = {
+        TableName: 'MessagingAppUsers'
+    };
+
+    docClient.scan(params, (err, data) => {
+        if(err) {
+            console.log(err);
         } else {
-            await User.find()
-            .then((laterResult) => {
-                laterResult.foreach((user) => {
-                    if(user.userID > maxID) {
-                        maxID = user.userID;
-                    }
-                })
-            })
-
-            const newUser = new User({
-                userName: req.params.userName,
-                userPassword: req.params.userPassword,
-                userID: maxID + 1
-            })
-
-            User.save()
-            .then((result) => {
-                res.redirect(`/${result.userID}/landing`);
-            })
+            console.log(data);
+            data.Items.forEach(element => {
+                console.log(element);
+                if(element.UserID > maxID) {
+                    maxID = element.UserID;
+                }
+            });
         }
     })
+
+    console.log(`Max ID: ${maxID}, Creating new user`);
+
+    var newUser = {
+        'UserID': maxID + 1,
+        'UserName': req.body.userName,
+        'UserPassword': req.body.password,
+    }
+
+    var newUserParams = {
+        TableName: 'MessagingAppUsers',
+        Item: newUser
+    }
+
+    console.log('Saving new User to DB');
+    console.log(newUser);
+
+    docClient.put(newUserParams, (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(`New User: ${newUser.UserName} Saved`);
+        }
+    })
+
+    res.redirect('landing');
 })
 
+console.log('Running');
 app.listen(3000);
